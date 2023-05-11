ebed08441cf67789ba57276e05666cd5b8ad2f59
ramirezramiro <ramiro.rodolfo.ramirez@gmail.com>
2023-05-11 17:24:30 +0800

    modify main

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	eth_read()
+	handler, err := connectToEthernetPort()
+	if err != nil {
+		log.Fatal("Error connecting to Ethernet port: ", err)
+	}
+	defer handler.Close()
+
+	data, err := readModbusData(handler)
+	if err != nil {
+		log.Fatal("Error reading Modbus data: ", err)
+	}
+
+	err = sendDataToMongoDB(data)
+	if err != nil {
+		log.Fatal("Error sending data to MongoDB: ", err)
+	}
+
+	log.Println("Modbus data saved to MongoDB successfully!")
 }
